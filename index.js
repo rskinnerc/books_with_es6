3041a37c5fa0cb8ddb1162ed219f48613e17da46
Ronald <rjskinnerc@unal.edu.co>
2022-04-25 12:30:16 -0500

    Add luxon package to show the date

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { BookForm } from "./modules/BookForm.js";
 import { library } from './modules/Library.js'
+import { DateTime } from "./node_modules/luxon/build/es6/luxon.js";
+
+const todayContainer = document.getElementById('today');
+todayContainer.textContent = DateTime.now().toFormat('DDD, tt');
 
 library.render();
 new BookForm();
